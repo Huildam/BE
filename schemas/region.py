1e6dd297cd9df5522163dd096a666fcaaa85fea3
10cheon00 <10cheon00@gmail.com>
2025-07-29 20:37:27 +0900

    fix: nested region fetch

--- schemas/region.py
+++ schemas/region.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class RegionResponse(BaseModel):
     id: int
     name: str
-    parent_id: int
+    parent: Optional['RegionResponse'] = None;
 
     class Config:
         orm_mode = True
